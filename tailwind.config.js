acc8deb03bb637580599b77be640a8f31146a8d7
Rasen08 <laura.analiacadiz@gmail.com>
2025-11-12 01:22:20 -0300

    Subiendo reorganización de carpetas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./public/index.html",
     "./**/*.html",
-    "./src/**/*.{js,ts}",
+    "./src/**/*.{js,ts,css}",
   ],
   theme: {
     extend: {
